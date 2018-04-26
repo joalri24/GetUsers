46b100c671152ebb238ee9fa4bbe7a104ad7a51e
Jose Riaño <jriano@2secure.org>
2018-04-26 10:35:53 -0500

    Connects to a unix machine and executes the pwd command.

--- GetUsers/Program.cs
+++ GetUsers/Program.cs
@@ -7,10 +7,29 @@ namespace GetUsers
     {
         static void Main(string[] args)
         {
-            
+
+
+            Console.WriteLine("Get Users!!");
+            var ConnInfo = new ConnectionInfo("x.x.x.x", 22, "fakeUser",
+                new AuthenticationMethod[] {
+
+                    new PasswordAuthenticationMethod("fakeUser", "fakePass")             
+                });
+
+            using (var sshclient = new SshClient(ConnInfo))
+            {
+                sshclient.Connect();
+                using (var cmd = sshclient.CreateCommand("pwd"))
+                {
+                    cmd.Execute();
+                    Console.WriteLine("Command>" + cmd.CommandText);
+                    Console.WriteLine("Return Value = {0}", cmd.Result);
+                }
+                sshclient.Disconnect();
+            }
+
             Console.WriteLine("Press any key to continue.");
             string address = Console.ReadLine();
-
         }
     }
 }
