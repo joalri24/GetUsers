f39468337fd11c3cb780b85f15352f6b47ba5c01
Jose Riaño <jriano@2secure.org>
2018-04-26 10:15:13 -0500

    Initial commit.

---
+++ GetUsers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Renci.SshNet;
+
+namespace GetUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Press any key to continue.");
+            string address = Console.ReadLine();
+
+        }
+    }
+}
